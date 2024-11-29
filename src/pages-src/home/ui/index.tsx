71268e3eef0f72bc9bd149b40ad9bba5113d75bc
avdeev <avd13.aaa@ya.ru>
2024-11-29 15:56:40 +0300

    правка

--- src/pages-src/home/ui/index.tsx
+++ src/pages-src/home/ui/index.tsx
@@ -1,21 +1,17 @@
-import { LinearProgress, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { cookies } from "next/headers";
-import { useBlogger } from "../api/use-blogger";
+import { TestElement } from "./test";
 
 export const Page = async () => {
   const cookieStore = await cookies();
   const ord_token = cookieStore.get("ord_token");
-
-  const { data, isLoading } = useBlogger();
 
   return (
     <>
       <Typography>Домашняя страница</Typography>
       <Typography>{`ord_token: ${JSON.stringify(ord_token)}`}</Typography>
       <br />
-      <Typography>Тест запроса</Typography>
-      {isLoading && <LinearProgress />}
-      <Typography>{`ответ: ${!!data ? JSON.stringify(data) : `-`}`}</Typography>
+      <TestElement />
     </>
   );
 };
