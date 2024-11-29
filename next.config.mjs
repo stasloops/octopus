cbeb3a6716991b7d0f11a0e707eea70af5e21cb1
avdeev <avd13.aaa@ya.ru>
2024-11-29 11:55:57 +0300

    пр

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,15 @@
 const nextConfig = {
   // output: 'standalone',
   // basePath: '/login',
-  transpilePackages: ['@mui/x-charts'],
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: { and: [/\.(js|ts)x?$/] },
-      use: ["@svgr/webpack"]
-    })
-    return config
-  }
+  // transpilePackages: ['@mui/x-charts'],
+  // webpack(config) {
+  //   config.module.rules.push({
+  //     test: /\.svg$/,
+  //     issuer: { and: [/\.(js|ts)x?$/] },
+  //     use: ["@svgr/webpack"]
+  //   })
+  //   return config
+  // }
 }
 
-module.exports = nextConfig
+export default nextConfig
