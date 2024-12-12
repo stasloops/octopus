2d2774780bc36ed8ce1ea9325d103cefb1d23edf
avdeev <avd13.aaa@ya.ru>
2024-12-12 13:03:42 +0300

    тест

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,8 +1,4 @@
-import {
-  verifySessionCustom
-} from "@/src/shared/lib/session-custom";
 import type { Metadata } from "next";
-import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
 export const metadata: Metadata = {
@@ -14,8 +10,8 @@ export default async function RootLayout({
 }: Readonly<{
   children: ReactNode;
 }>) {
-  const session = await verifySessionCustom();
-  if (!session) redirect("/login");
+  // const session = await verifySessionCustom();
+  // if (!session) redirect("/login");
 
   return <>{children}</>;
 }
