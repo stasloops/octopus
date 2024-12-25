c1b38c6021bffc5fdb5ff6c2a4cd443b597304a2
avdeev <avd13.aaa@ya.ru>
2024-12-25 13:22:10 +0300

    скролл вверх

--- src/pages-src/blogger/vk/ui/index.tsx
+++ src/pages-src/blogger/vk/ui/index.tsx
@@ -2,8 +2,16 @@
 
 import Loading from "@/src/pages-src/loading";
 import { Layout } from "@/src/widgets/layout";
-import { Box, Chip, Stack, Tooltip, Typography } from "@mui/material";
-import { FC } from "react";
+import ArrowCircleUpOutlinedIcon from "@mui/icons-material/ArrowCircleUpOutlined";
+import {
+  Box,
+  Chip,
+  IconButton,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { FC, useEffect, useState } from "react";
 import { useGetBloggerMutate } from "../api/use-blogger";
 import { useGetBloggerMutateStats } from "../api/use-blogger-stats";
 import { Charts } from "./charts";
@@ -21,11 +29,36 @@ export const Page: FC<PageProps> = ({ idBlogger }) => {
     useGetBloggerMutateStats();
   const isLoading = isLoadingBlogger && isLoadingBloggerStats;
 
+  const [scrollUp, setScrollUp] = useState<boolean>(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const scrollTop = document.documentElement.scrollTop;
+      if (scrollTop > 100) {
+        setScrollUp(true);
+      } else {
+        setScrollUp(false);
+      }
+    }, 200);
+    return () => clearInterval(interval);
+  });
+
+  const onClickUp = () => {
+    document.documentElement.scroll(0, 0);
+  };
+
   return (
     <>
       {!!dataBlogger && !isLoading && (
         <>
           <Layout />
+          {scrollUp && (
+            <Box sx={{ position: `fixed`, bottom: 0, right: 0 }}>
+              <IconButton size="large" onClick={onClickUp}>
+                <ArrowCircleUpOutlinedIcon fontSize="large" />
+              </IconButton>
+            </Box>
+          )}
           <Box>
             <Stack
               direction="row"
