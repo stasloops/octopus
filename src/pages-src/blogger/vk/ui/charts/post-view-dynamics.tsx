ce2993eadff9e9412086ff7f1afd0bd044e5f4d1
avdeev <avd13.aaa@ya.ru>
2025-01-25 16:25:12 +0300

    дизайн графиков столбцев

--- src/pages-src/blogger/vk/ui/charts/post-view-dynamics.tsx
+++ src/pages-src/blogger/vk/ui/charts/post-view-dynamics.tsx
@@ -2,7 +2,12 @@ import { fakerRU } from "@faker-js/faker";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Box, IconButton, Paper, Stack, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
-import { BarChart, PieValueType } from "@mui/x-charts";
+import {
+  axisClasses,
+  BarChart,
+  barElementClasses,
+  PieValueType,
+} from "@mui/x-charts";
 import { MakeOptional } from "@mui/x-charts/internals";
 import { FC, useMemo } from "react";
 import { useGetBloggerMutateStats } from "../../api/use-blogger-stats";
@@ -83,6 +88,7 @@ export const PostViewDynamics: FC = () => {
                     height: `100%`,
                     ...(dataChart.length < 5
                       ? {
+                          top: `25%`,
                           height: `50%`,
                         }
                       : {}),
@@ -109,7 +115,147 @@ const ChartElement: FC<{
         series={[{ data: dataChart.map((el) => el.value) }]}
         margin={{ left: 65, top: 10 }}
         layout="horizontal"
-      />
+        sx={(theme) => ({
+          "--my-custom-gradient-2": "url(#GlobalGradient2)",
+          [`.${barElementClasses.root}`]: {
+            fill: `#fff0`,
+            // strokeWidth: 4,
+            // stroke: theme.palette.primary.contrastText,
+          },
+          [`.${axisClasses.root}`]: {
+            [`.${axisClasses.line}`]: {
+              stroke: `#B5CDEF`,
+              strokeWidth: `8.75px`,
+            },
+            [`.${axisClasses.tick}`]: {
+              stroke: `#2B69D5`,
+              strokeWidth: `8.75px`,
+            },
+            [`.${axisClasses.tickLabel}`]: {
+              fill: `#222657`,
+              // fontSize: `30px !important`,
+            },
+          },
+          [`.${axisClasses.directionX} .${axisClasses.tickContainer}:last-child .MuiChartsAxis-tick`]:
+            {
+              display: `none`,
+            },
+        })}
+        slotProps={{
+          popper: {
+            sx: {
+              "--my-custom-gradient-2":
+                "linear-gradient(90.36deg, #2B3ABB 3.14%, #70D2F8 99.72%)",
+            },
+          },
+          axisTickLabel: {
+            fontSize: `30px`,
+          },
+        }}
+        slots={{
+          bar: (props) => {
+            const radius = 7;
+            // const { ownerState } = props;
+            const { x, y, height, width, ...restProps } = props.style as any;
+            if (x?.animation?.to === undefined) return;
+            const d = `M${x.animation.to},${y.animation.to} h${
+              width.animation.to - radius
+            } a${radius},${radius} 0 0 1 ${radius},${radius}v ${
+              height.animation.to - 2 * radius
+            } a${radius},${radius} 0 0 1 ${-radius},${radius} h${
+              radius - width.animation.to
+            }z`;
+
+            return (
+              <path
+                d={d}
+                fill={`var(--my-custom-gradient-2, #123456)`}
+                {...restProps}
+              />
+            );
+          },
+          axisLine: (props) => {
+            return (
+              <g>
+                <line
+                  {...props}
+                  {...(typeof props.x1 === "number" && { x1: props.x1 - 10 })}
+                  {...(typeof props.y2 === "number" && { y2: props.y2 + 10 })}
+                  strokeLinecap="round"
+                  stroke="#B5CDEF"
+                  strokeWidth="8.75px"
+                />
+                {typeof props.x2 === "number" && (
+                  <line
+                    x1={props.x2}
+                    x2={props.x2}
+                    y1={0}
+                    y2={10}
+                    strokeLinecap="round"
+                    stroke="#B5CDEF"
+                    strokeWidth="8.75px"
+                    style={{ zIndex: 2 }}
+                  />
+                )}
+                {typeof props.y1 === "number" && (
+                  <line
+                    y1={props.y1}
+                    y2={props.y1}
+                    x1={0}
+                    x2={-10}
+                    strokeLinecap="round"
+                    stroke="#B5CDEF"
+                    strokeWidth="8.75px"
+                  />
+                )}
+              </g>
+            );
+          },
+          axisTick: (props) => {
+            if (typeof props.x2 === "number") return null;
+            return (
+              <g>
+                <line
+                  {...props}
+                  strokeLinecap="round"
+                  {...(typeof props.y2 === "number" && {
+                    y1: -4,
+                    y2: props.y2,
+                  })}
+                />
+              </g>
+            );
+          },
+          axisTickLabel: (props) => {
+            return (
+              <g>
+                <text {...props}>
+                  {props.x == 0 ? (
+                    <tspan x="0" dy="9px" dominantBaseline="hanging">
+                      {props.text}
+                    </tspan>
+                  ) : (
+                    <tspan x="-10" dy="-6px" dominantBaseline="hanging">
+                      {props.text}
+                    </tspan>
+                  )}
+                </text>
+              </g>
+            );
+          },
+        }}
+      >
+        <linearGradient
+          id="GlobalGradient2"
+          x1="0%"
+          y1="100%"
+          x2="100%"
+          y2="0%"
+        >
+          <stop offset="0" stopColor="#2B3ABB" />
+          <stop offset="1" stopColor="#70D2F8" />
+        </linearGradient>
+      </BarChart>
     </>
   );
 };
