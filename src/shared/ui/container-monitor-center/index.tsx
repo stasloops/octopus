9bafaba1ddfc5d48f195a459d310701c2e91f91f
avdeev <avd13.aaa@ya.ru>
2025-01-27 16:08:10 +0300

    правка

--- src/shared/ui/container-monitor-center/index.tsx
+++ src/shared/ui/container-monitor-center/index.tsx
@@ -1,24 +1,5 @@
-import { Box, Stack } from "@mui/material";
 import { FC, PropsWithChildren } from "react";
 
 export const ConteinerMonitorCenter: FC<PropsWithChildren> = ({ children }) => {
-  return (
-    <Stack
-      direction="row"
-      sx={{
-        width: `100%`,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Box
-        sx={{
-          width: `100%`,
-          maxWidth: `1204px`,
-        }}
-      >
-        {children}
-      </Box>
-    </Stack>
-  );
+  return <>{children}</>;
 };
